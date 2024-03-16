61cb0939945f2e24564c07844021733a5bd8b1f0
lilianmueninzioki <liliannzioki@gmail.com>
2024-03-16 23:35:36 +0300

    calculator

---
+++ index.js
@@ -0,0 +1,41 @@
+function add( a,b) {
+    return a+b;
+}
+console.log(add);
+
+function subtract(a,b) {
+    return a-b;
+}
+console.log(subtract);
+
+function multiply(a,b){
+    return a*b;
+}
+console.log(multiply);
+
+function divide(a,b) {
+    return a/b;
+}
+console.log(divide);
+
+function increment(n) {
+    return n+=1;
+}
+console.log(increment);
+
+function decrement(n) {
+    return n-=1;   
+}
+console.log(decrement);
+
+function makeInt(n) {
+    return parseInt(n,10);
+}
+console.log(makeInt);
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+console.log(preserveDecimal);
+
+
